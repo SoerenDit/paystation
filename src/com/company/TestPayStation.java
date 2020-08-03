1d5a57a66dbe6d7210d8e4609634d91862db7f4e
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2020-08-03 17:51:32 +0200

    shouldDisplay10MinFor25Cents test added and passed

--- src/com/company/TestPayStation.java
+++ src/com/company/TestPayStation.java
@@ -4,11 +4,24 @@ import static org.junit.Assert.*;
 
 
 public class TestPayStation {
+    PayStationImp ps;
+
+    @Before
+    public void setUp() {
+        ps = new PayStationImp();
+    }
 
     @Test
     public void shouldDisplay2MinFor5Cents() throws IllegalCoinException {
-        PayStationImp ps = new PayStationImp();
         ps.addPayment(5);
         assertEquals("Should display 2 min for 5 cents", 2, ps.readDisplay());
     }
+
+    @Test
+    public void shouldDisplay10MinFor25Cents() throws IllegalCoinException {
+        ps.addPayment(25);
+        assertEquals("Should display 10 min for 25 cents", 25 / 5 * 2, ps.readDisplay());
+    }
+
+
 }
