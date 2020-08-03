adbeb774d0663a025743df8985a6ddef2ec69605
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2020-08-03 17:30:49 +0200

    Main class changed to class TestPayStation

---
+++ src/com/company/TestPayStation.java
@@ -0,0 +1,14 @@
+package com.company;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class TestPayStation {
+
+    @Test
+    public void shouldDisplay2MinFor5Cents() throws IllegalCoinException {
+        PayStation ps = new PayStation;
+        ps.addPayment(5);
+        assertEquals("Should display 2 min for 5 cents", 2, ps.readDisplay());
+    }
+}
